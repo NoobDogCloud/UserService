f3b286d0c4103f264bb23d4d31d5b5dc6a66029c
putao520 <yuyao1022@hotmail.com>
2022-05-25 15:27:38 +0800

    补充会话设定

--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -1,6 +1,7 @@
 package main.java.Main;
 
 import common.java.JGrapeSystem.GscBooster;
+import common.java.Session.UserSession;
 
 // -n test -h 127.0.0.1:805
 public class main {
@@ -9,6 +10,8 @@ public class main {
         // 调试时需要配置运行参数 例如: -n test-d -h 127.0.0.1:805 -k grapeSoft@ -p 123456
         // -n 服务名称 -h 主控服务器地址 -k 主控服务器密钥 -p 端口
         System.out.println("<Hello World服务>");
+        // 设置会话系统使用 jwt
+        UserSession.setDefaultDriver(UserSession.JwtDriver);
         GscBooster.start(args, () -> {
             // 测试分布式订阅源系统
             // SubscribeGsc.injectDistribution();
