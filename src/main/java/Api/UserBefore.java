41d6702687431ef36f07c7ab6f0ba39ee3203202
putao520 <yuyao1022@hotmail.com>
2022-05-25 14:20:11 +0800

    init

--- src/main/java/Api/UserBefore.java
+++ src/main/java/Api/UserBefore.java
@@ -13,12 +13,16 @@ public class UserBefore extends RpcBefore {
             FilterReturn r = RpcJsonFilterHelper.build(data, isUpdate)
                     // 新增时,判断是否包含密码校验,并生成salt,加密密码
                     .filter("password", (info, name) -> {
-                        if (!data.get("password").equals("password1")) {
+                        if (!data.get("password").equals(data.get("password1"))) {
                             return FilterReturn.fail("输入的2次密码不一致");
                         }
                         String salt = StringHelper.randomString(8);
                         data.put("password", UserModel.EncodePassword(info.getString("user_id"), info.getString("password"), salt));
                         return FilterReturn.success();
+                    })
+                    .filterUnique("user_id", line -> {
+                        var api = new User();
+                        return api.getPureDB().limit(2).eq("user_id", line.getString("user_id")).select();
                     })
                     .filter("group_id", (info, name) -> UserModel.checkRoles(info.getString("group_id")) ? FilterReturn.success() : FilterReturn.fail("无效角色"),
                             true,
--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -6,8 +6,8 @@ import common.java.JGrapeSystem.GscBooster;
 public class main {
     public static void main(String[] args) {
         // 修改服务显示名称
-        // 调试时需要配置运行参数 例如: -n test-d -h 127.0.0.1:805 -k grapeSoft@
-        // -n 服务名称 -h 主控服务器地址 -k 主控服务器密钥
+        // 调试时需要配置运行参数 例如: -n test-d -h 127.0.0.1:805 -k grapeSoft@ -p 123456
+        // -n 服务名称 -h 主控服务器地址 -k 主控服务器密钥 -p 端口
         System.out.println("<Hello World服务>");
         GscBooster.start(args, () -> {
             // 测试分布式订阅源系统
