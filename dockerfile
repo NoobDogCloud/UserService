836d7c0a01ba930d0451cc860a501e02baf6b34c
putao520 <yuyao1022@hotmail.com>
2022-06-23 00:11:15 +0800

    完善服务更新脚手架

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:19-jdk-alpine3.16
-COPY ./target/user-service-3.0.0-jar-with-dependencies.jar /home/app/
+COPY ./target/#{f} /home/app/
 WORKDIR /home/app
-ENTRYPOINT ["java", "-Dfile.encoding=utf-8", "-jar", "user-service-3.0.0-jar-with-dependencies.jar"]
+ENTRYPOINT ["java", "-Dfile.encoding=utf-8", "-jar", "#{f}"]
--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -9,7 +9,7 @@ public class main {
         // 修改服务显示名称
         // 调试时需要配置运行参数 例如: -n test-d -h 127.0.0.1:805 -k grapeSoft@ -p 123456
         // -n 服务名称 -h 主控服务器地址 -k 主控服务器密钥 -p 端口
-        System.out.println("<Hello World服务>");
+        System.out.println("<标准用户服务>");
         // 设置会话系统使用 jwt
         UserSession.setDefaultDriver(UserSession.JwtDriver);
         GscBooster.start(args, () -> {
